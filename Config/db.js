76a648b9f3fc7db009ca17145e939f32d50560e3
GreatA-Maker <75976912+GreatA-Maker@users.noreply.github.com>
2023-01-21 03:53:42 +0530

    deployment render changed

--- Config/db.js
+++ Config/db.js
@@ -2,5 +2,6 @@ const mongoose = require("mongoose");
 
 require("dotenv").config();
 
+mongoose.set("strictQuery", false);
 const connection = mongoose.connect(process.env.MONGO_URL);
 module.exports = connection;
